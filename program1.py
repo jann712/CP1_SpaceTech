8c25ae7233142f8bdf4c56940f66fccc43fb53f4
jann712 <joao.augustonn1@gmail.com>
2026-09-07 13:01:08 -0300

    added battery analysis

--- program1.py
+++ program1.py
@@ -8,8 +8,7 @@ from kit_dados import geracao_solar, baterias
 # 2. porcentagem da poeira x geracao_kw (interrupção ambiental)
 
 #indicadores baterias
-# 3. eficiência da bateria em relação à saude (capacidade_kwh / saude_pct)
-# 4. quantidade total da bateria (capacidade_kwh x 100 -carga_pct )
+# 3. eficiência da bateria em relação à saude (capacidade_kwh / saude_pct)=
 
 def analise_solar():
 
@@ -52,9 +51,31 @@ def analise_solar():
 analise_solar()
 
 def analise_bateria():
+
+    contagem_atencao = 0
+    contagem_critico = 0
+
+    print("===========================================================================")  
+
     for registro in baterias:
         eficiencia = round(registro["capacidade_kwh"]/registro["saude_pct"], 2)
         print ('Eficiência da bateria em relação à saúde', registro["id"], eficiencia)
+
+        if eficiencia < 3: 
+            print("ÍNDICE DE SAÚDE DA BATERIA: CRÍTICO")
+            contagem_critico += 1
+        
+        elif eficiencia < 4: 
+            print("ÍNDICE DE SAÚDE DA BATERIA: ATENÇÃO")
+            contagem_atencao += 1
+        
+        elif eficiencia >= 5: print("ÍNDICE DE SAÚDE DA BATERIA: NORMAL")
+
+        print("===========================================================================")  
+
+    if contagem_atencao > 0 or contagem_critico > 0:
+        print("!!!Realize a troca das baterias com saúde degradada!!!\n")         
+        
 
 analise_bateria()
 
